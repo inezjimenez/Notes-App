84f0805b59b7a215fb97407662490a3c5c31b4b2
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-08-06 18:28:13 -0500

    updates to all files

--- develop/routes/htmlRoutes.js
+++ develop/routes/htmlRoutes.js
@@ -2,9 +2,9 @@
 const path = require("path");
 const router = require('express').Router();
 
-router.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'))
-});
+// router.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, './develop/public/index.html'))
+// });
 
 router.get('/notes',(req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/notes.html'))
